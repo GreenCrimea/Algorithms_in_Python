78b751d60cdb304e93d4bad0b0660c1145e4e001
greencrimea <tom.horton100@gmail.com>
2022-10-06 13:37:38 +1100

    .

--- main.py
+++ main.py
@@ -147,3 +147,58 @@ def insertion_sort(arr):
     return arr
 
 #print(insertion_sort(arr))
+
+
+#linked list
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    
+    def traversal(self):
+        first = self.head
+        while first:
+            print(first.data)
+            first = first.next
+
+    def insert_new_header(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head 
+        self.head = new_node
+
+    def search(self, x):
+        temp = self.head
+        while temp is not None:
+            if temp.data == x:
+                return True
+            else:
+                return False
+
+    def delete(self, data):
+        temp = self.head
+        while temp is not None:
+            if temp.data == data:
+                break
+            prev = temp
+            temp = temp.next
+            prev.next = temp.next
+
+
+family = LinkedList()
+family.head = Node("Bob")
+wife = Node("Amy")
+first_kid = Node("Max")
+second_kid = Node("Jenny")
+
+family.head.next = wife
+wife.next = first_kid
+first_kid.next = second_kid
+
+family.insert_new_header("Dave")
+family.delete("Bob")
+#family.traversal()
