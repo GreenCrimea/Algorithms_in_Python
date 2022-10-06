392a7aa09f8c6d56ba6865d2a646ad6d8b12d27c
greencrimea <tom.horton100@gmail.com>
2022-10-06 14:27:23 +1100

    .

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from math import factorial
+import numpy as np
 
 
 '''
@@ -239,3 +240,71 @@ arr = [13, 43, 45, 35, 46, 35, 46, 44, 52, 35, 55, 44, 56, 72, 52, 34, 56 , 37, 
 
 #matrix multiplication
 
+x = [[1, 2],
+     [2, 3]]
+
+y = [[2, 3],
+     [3, 4]]
+
+result = [[0, 0],
+          [0, 0]]
+
+def matrix_multiply(x, y, result):
+    for i in range(len(x)):
+        for j in range(len(y[0])):
+            for k in range(len(y)):
+                result[i][j] += x[i][k] * y[k][j]
+    return result
+
+#print(matrix_multiply(x, y, result))
+
+
+#strassen
+
+x = np.array([[1, 2], [2, 3]])
+y = np.array([[2, 3], [3, 4]])
+
+#iterative
+def strassen_iter(x, y):
+    a, b, c, d = x[0, 0], x[0, 1], x[1, 0], x[1, 1]
+    e, f, g, h = y[0, 0], y[0, 1], y[1, 0], y[1, 1]
+
+    p1 = a * (f - h)
+    p2 = (a + b) * h
+    p3 = (c + d) * e
+    p4 = d * (g - e)
+    p5 = (a + d) * (e + h)
+    p6 = (b - d) * (g + h)
+    p7 = (a - c) * (e + f)
+
+    c1 = (p5 + p4 - p2 + p6)
+    c2 = (p1 + p2)
+    c3 = (p3 + p4)
+    c4 = (p1 + p5 - p3 - p7)
+
+    return np.array([[c1, c2], [c3, c4]])
+
+#print(strassen_iter(x, y))
+
+#recursive
+def strassen_recur(x, y):
+    a, b, c, d = x[0, 0], x[0, 1], x[1, 0], x[1, 1]
+    e, f, g, h = y[0, 0], y[0, 1], y[1, 0], y[1, 1]
+
+    p1 = strassen_recur(a, f - h)
+    p2 = strassen_recur(a + b, h)
+    p3 = strassen_recur(c + d, e)
+    p4 = strassen_recur(d, g - e)
+    p5 = strassen_recur(a + d, e + h)
+    p6 = strassen_recur(b - d, g + h)
+    p7 = strassen_recur(a - c, e + f)
+
+    c1 = (p5 + p4 - p2 + p6)
+    c2 = (p1 + p2)
+    c3 = (p3 + p4)
+    c4 = (p1 + p5 - p3 - p7)
+
+    c = np.vstack((np.hstack((c1, c2)), np.hstack((c3, c4))))
+    return c
+
+#print(strassen_recur(x, y))
